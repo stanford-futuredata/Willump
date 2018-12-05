063f23bcf33786c1962e56c1c6dc2298385e7cc9
Peter Kraft <petereliaskraft@gmail.com>
2018-12-05 11:59:30 -0800

    Cleanup graph_builder so functions return None instead of panicking.

--- willump/evaluation/willump_graph_builder.py
+++ willump/evaluation/willump_graph_builder.py
@@ -14,6 +14,7 @@ from willump.graph.logistic_regression_node import LogisticRegressionNode
 from willump.graph.array_append_node import ArrayAppendNode
 
 from typing import MutableMapping, List, Tuple, Optional, Set, Mapping
+import typing
 from weld.types import *
 
 
@@ -55,17 +56,46 @@ class WillumpGraphBuilder(ast.NodeVisitor):
     def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
         """
         Begin processing of a function.  Create input nodes for function arguments.
-
-        Assumes all function arguments are numpy float64 arrays.
         """
         for arg in node.args.args:
             arg_name: str = arg.arg
             input_node: WillumpInputNode = WillumpInputNode(arg_name)
             self._node_dict[arg_name] = input_node
             self.arg_list.append(arg_name)
-        ast.NodeVisitor.generic_visit(self, node)
+        for entry in node.body:
+            if isinstance(entry, ast.Assign):
+                result = self.analyze_Assign(entry)
+                if result is None:
+                    panic("Unrecognized assign %s" % ast.dump(entry))
+                else:
+                    output_var_name, node_or_mathop = result
+                    if node_or_mathop is None:
+                        pass
+                    elif isinstance(node_or_mathop, WillumpGraphNode):
+                        self._node_dict[output_var_name] = node_or_mathop
+                    else:
+                        self._mathops_list.append(node_or_mathop)
+            elif isinstance(entry, ast.Return):
+                self.visit_Return(entry)
+            elif isinstance(entry, ast.For):
+                for for_entry in entry.body:
+                    if isinstance(for_entry, ast.Assign):
+                        result = self.analyze_Assign(for_entry)
+                        if result is None:
+                            panic("Unrecognized assign %s" % ast.dump(for_entry))
+                        else:
+                            output_var_name, node_or_mathop = result
+                            if node_or_mathop is None:
+                                pass
+                            elif isinstance(node_or_mathop, WillumpGraphNode):
+                                self._node_dict[output_var_name] = node_or_mathop
+                            else:
+                                self._mathops_list.append(node_or_mathop)
+            else:
+                panic("Unrecognized body node %s" % ast.dump(entry))
 
-    def visit_Assign(self, node: ast.Assign) -> None:
+    def analyze_Assign(self, node: ast.Assign) -> \
+            Optional[Tuple[str, Optional[typing.Union[WillumpGraphNode, MathOperation]]]]:
         """
         Process an assignment AST node into either a Willump node or MathOperation that
         defines the variable being assigned.
@@ -73,37 +103,47 @@ class WillumpGraphBuilder(ast.NodeVisitor):
         assert(len(node.targets) == 1)  # Assume assignment to only one variable.
         target: ast.expr = node.targets[0]
         output_var_name = self.get_assignment_target_name(target)
+        if output_var_name is None:
+            return None
         output_type = self._type_map[output_var_name]
         value: ast.expr = node.value
         if isinstance(value, ast.BinOp):
-            left, op, right = self._analyze_binop(value)
+            analyzed_binop = self._analyze_binop(value)
+            if analyzed_binop is None:
+                return None
+            else:
+                left, op, right = analyzed_binop
             binop_mathop: MathOperation = MathOperation(op, output_var_name, output_type,
                                                         left, second_input=right)
-            self._mathops_list.append(binop_mathop)
+            return output_var_name, binop_mathop
         elif isinstance(value, ast.Call):
-            called_function: str = self._get_function_name(value)
+            called_function: Optional[str] = self._get_function_name(value)
+            if called_function is None:
+                return None
             # Process unary math operations into MathOperations
-            if self._pyunop_to_wunop(called_function) != "":
+            if self._pyunop_to_wunop(called_function) is not None:
                 assert(len(value.args) == 1)  # Assume unary operation.
-                operator: str = self._pyunop_to_wunop(called_function)
-                unary_input: MathOperationInput = self._expr_to_math_operation_input(value.args[0])
+                operator: Optional[str] = self._pyunop_to_wunop(called_function)
+                unary_input: Optional[MathOperationInput] = self._expr_to_math_operation_input(value.args[0])
+                if operator is None or unary_input is None:
+                    return None
                 unary_mathop: MathOperation = MathOperation(operator, output_var_name,
                                                             output_type, unary_input)
-                self._mathops_list.append(unary_mathop)
+                return output_var_name, unary_mathop
             # TODO:  Recognize functions in attributes properly.
             elif "split" in called_function:
                 split_input_var: str = value.func.value.id
                 split_input_node: WillumpGraphNode = self._node_dict[split_input_var]
                 string_split_node: StringSplitNode = StringSplitNode(input_node=split_input_node,
                                                                      output_name=output_var_name)
-                self._node_dict[output_var_name] = string_split_node
+                return output_var_name, string_split_node
             # TODO:  Recognize when this is being called in a loop, don't just assume it is.
             elif "lower" in called_function:
                 lower_input_var: str = value.func.value.value.id
                 lower_input_node: WillumpGraphNode = self._node_dict[lower_input_var]
                 string_lower_node: StringLowerNode = StringLowerNode(input_node=lower_input_node,
                                                                      output_name=output_var_name)
-                self._node_dict[output_var_name] = string_lower_node
+                return output_var_name, string_lower_node
             # TODO:  Recognize replaces that do more than remove one character.
             elif "replace" in called_function:
                 replace_input_var: str = value.func.value.value.id
@@ -114,7 +154,7 @@ class WillumpGraphBuilder(ast.NodeVisitor):
                 string_remove_char_node: StringRemoveCharNode =\
                     StringRemoveCharNode(input_node=replace_input_node,
                     target_char=target_char, output_name=output_var_name)
-                self._node_dict[output_var_name] = string_remove_char_node
+                return output_var_name, string_remove_char_node
             # TODO:  Find a real function to use here.
             elif "willump_frequency_count" in called_function:
                 freq_count_input_var: str = value.args[0].id
@@ -124,7 +164,7 @@ class WillumpGraphBuilder(ast.NodeVisitor):
                     input_node=freq_count_input_node, output_name=output_var_name,
                     input_vocab_dict=vocab_dict, aux_data=self.aux_data
                 )
-                self._node_dict[output_var_name] = vocab_freq_count_node
+                return output_var_name, vocab_freq_count_node
             # TODO:  Lots of potential predictors, differentiate them!
             elif "predict" in called_function:
                 logit_input_var: str = value.args[0].id
@@ -136,7 +176,7 @@ class WillumpGraphBuilder(ast.NodeVisitor):
                     logit_weights=logit_weights,
                     logit_intercept=logit_intercept, aux_data=self.aux_data
                 )
-                self._node_dict[output_var_name] = logit_node
+                return output_var_name, logit_node
             # TODO:  Support values that are not scalar variables.
             elif "numpy.append" in called_function:
                 append_input_array: str = value.args[0].id
@@ -146,17 +186,16 @@ class WillumpGraphBuilder(ast.NodeVisitor):
                 array_append_node: ArrayAppendNode = ArrayAppendNode(append_input_array_node, append_input_val_node,
                                                                 output_var_name, self._type_map[append_input_array],
                                                                 self._type_map[output_var_name])
-                self._node_dict[output_var_name] = array_append_node
+                return output_var_name, array_append_node
             # TODO:  What to do with these?
             elif called_function == "numpy.zeros":
-                pass
+                return output_var_name, None
             elif "reshape" in called_function:
-                pass
+                return output_var_name, None
             else:
-                panic("Unrecognized function call {0} node {1}".format(called_function,
-                                                                       ast.dump(value)))
+                return None
         else:
-            panic("Unrecognized expression {0}".format(ast.dump(value)))
+            return None
 
     def visit_Return(self, node: ast.Return) -> None:
         """
@@ -181,9 +220,6 @@ class WillumpGraphBuilder(ast.NodeVisitor):
         else:
             panic("Unrecognized return: {0}".format(ast.dump(node)))
 
-    def generic_visit(self, node) -> None:
-        ast.NodeVisitor.generic_visit(self, node)
-
     def get_willump_graph(self) -> WillumpGraph:
         return self.willump_graph
 
@@ -193,7 +229,7 @@ class WillumpGraphBuilder(ast.NodeVisitor):
     def get_aux_data(self) -> List[Tuple[int, WeldType]]:
         return self.aux_data
 
-    def _expr_to_math_operation_input(self, expr: ast.expr) -> MathOperationInput:
+    def _expr_to_math_operation_input(self, expr: ast.expr) -> Optional[MathOperationInput]:
         """
         Convert an expression input to a binary or unary operation into a MathOperationInput.
 
@@ -207,7 +243,9 @@ class WillumpGraphBuilder(ast.NodeVisitor):
             else:
                 return MathOperationInput(WeldInt(), input_literal=expr.n)
         elif isinstance(expr, ast.Subscript):
-            var_name: str = self.get_assignment_target_name(expr)
+            var_name: Optional[str] = self.get_assignment_target_name(expr)
+            if var_name is None:
+                return None
             return MathOperationInput(self._type_map[var_name],
                                       input_index=(var_name, expr.slice.value.n))
         elif isinstance(expr, ast.Name):
@@ -220,23 +258,29 @@ class WillumpGraphBuilder(ast.NodeVisitor):
             fake_assign: ast.Assign = ast.Assign()
             fake_assign.targets = [temp_target]
             fake_assign.value = expr
-            self.visit_Assign(fake_assign)
-            return MathOperationInput(WeldDouble(), input_var=temp_target.id)
+            result = self.analyze_Assign(fake_assign)
+            if result is None:
+                return None
+            else:
+                _, mathop = result
+                self._mathops_list.append(mathop)
+                return MathOperationInput(WeldDouble(), input_var=temp_target.id)
 
     def _analyze_binop(self, binop: ast.BinOp) -> \
-            Tuple[MathOperationInput, str, MathOperationInput]:
+            Optional[Tuple[MathOperationInput, str, MathOperationInput]]:
         """
         Convert an AST binop into two MathOperationInputs and an operator-string for a Transform
         Math Node.
         """
-        operator: str = self._pybinop_to_wbinop(binop.op)
-        if operator == "":
-            panic("Unsupported binop {0}".format(ast.dump(binop)))
+        operator: Optional[str] = self._pybinop_to_wbinop(binop.op)
         left_expr: ast.expr = binop.left
-        left_input: MathOperationInput = self._expr_to_math_operation_input(left_expr)
+        left_input: Optional[MathOperationInput] = self._expr_to_math_operation_input(left_expr)
         right_expr: ast.expr = binop.right
-        right_input: MathOperationInput = self._expr_to_math_operation_input(right_expr)
-        return left_input, operator, right_input
+        right_input: Optional[MathOperationInput] = self._expr_to_math_operation_input(right_expr)
+        if left_input is None or right_input is None or operator is None:
+            return None
+        else:
+            return left_input, operator, right_input
 
     def _build_math_transform_for_output(self, output_name: str) -> Optional[TransformMathNode]:
         """
@@ -270,7 +314,7 @@ class WillumpGraphBuilder(ast.NodeVisitor):
                     node: Optional[TransformMathNode] =\
                         self._build_math_transform_for_output(mathop_output)
                     if node is None:
-                        panic("No way to build {0} is found".format(mathop_output))
+                        return None
         # Return None if the final output is not buildable.
         if len(node_mathop_list) == 0:
             return None
@@ -292,7 +336,7 @@ class WillumpGraphBuilder(ast.NodeVisitor):
         return _temp_var_name
 
     @staticmethod
-    def get_assignment_target_name(node: ast.expr) -> str:
+    def get_assignment_target_name(node: ast.expr) -> Optional[str]:
         """
         Return the name of the target of an assignment statement.
         """
@@ -301,30 +345,32 @@ class WillumpGraphBuilder(ast.NodeVisitor):
         elif isinstance(node, ast.Subscript):
             return WillumpGraphBuilder.get_assignment_target_name(node.value)
         else:
-            panic("Unrecognized assignment to: {0}".format(ast.dump(node)))
-            return ""
+            return None
 
     @staticmethod
-    def _get_function_name(call: ast.Call) -> str:
+    def _get_function_name(call: ast.Call) -> Optional[str]:
         """
         Get the name of a function being called from a Call node.
 
         TODO:  Handle the ways different import statements can affect names. (e.g. numpy vs np).
         """
-        def _get_layer_name(func) -> str:
+        def _get_layer_name(func) -> Optional[str]:
             if isinstance(func, ast.Name):
                 return func.id
             elif isinstance(func, ast.Attribute):
-                return _get_layer_name(func.value) + "." + func.attr
+                next_name = _get_layer_name(func.value)
+                if next_name is None:
+                    return None
+                else:
+                    return next_name + "." + func.attr
             elif isinstance(func, ast.Subscript):
                 return _get_layer_name(func.value)
             else:
-                panic("Unrecognized node in function call {0}".format(ast.dump(func)))
-                return ""
+                return None
         return _get_layer_name(call.func)
 
     @staticmethod
-    def _pybinop_to_wbinop(binop: ast.operator) -> str:
+    def _pybinop_to_wbinop(binop: ast.operator) -> Optional[str]:
         """
         Convert from AST binops to strings for TransformMathNode.
         """
@@ -337,10 +383,10 @@ class WillumpGraphBuilder(ast.NodeVisitor):
         elif isinstance(binop, ast.Div):
             return "/"
         else:
-            return ""
+            return None
 
     @staticmethod
-    def _pyunop_to_wunop(unop: str) -> str:
+    def _pyunop_to_wunop(unop: str) -> Optional[str]:
         """
         Convert from Python function unops to strings for TransformMathModule.
         """
@@ -349,4 +395,4 @@ class WillumpGraphBuilder(ast.NodeVisitor):
         elif unop == "math.log":
             return "log"
         else:
-            return ""
+            return None
