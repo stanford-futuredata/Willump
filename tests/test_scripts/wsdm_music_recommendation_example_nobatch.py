865c08a775c5362b7639c40a83d58f824d7f33a4
Peter Kraft <petereliaskraft@gmail.com>
2019-01-09 17:52:32 -0800

    WSDM Cup Join examples

---
+++ tests/test_scripts/wsdm_music_recommendation_example_nobatch.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+import time
+import gc
+
+# LATENT VECTOR SIZES
+UF_SIZE = 32
+UF2_SIZE = 32
+SF_SIZE = 32
+AF_SIZE = 32
+
+
+def load_combi_prep(folder='data_new/', split=None):
+    print('LOAD COMBI')
+
+    start = time.time()
+
+    name = 'combi_extra' + ('.' + str(split) if split is not None else '') + '.pkl'
+    combi = pd.read_pickle(folder + name)
+
+    print('loaded in: ', (time.time() - start))
+
+    return combi
+
+
+def do_merge(combi, features, how, on):
+    combi = combi.merge(features, how=how, on=on)
+    return combi
+
+
+def add_als(folder, combi, size, user=True, pos=False, postfix='', artist=False):
+    if user:
+        if artist:
+            name = 'user2'
+            key = 'uf2_'
+        else:
+            name = 'user'
+            key = 'uf_'
+    else:
+        if artist:
+            name = 'artist'
+            key = 'af_'
+        else:
+            name = 'song'
+            key = 'sf_'
+    csv_name = folder + 'als' + postfix + '_' + name + '_features' + ('_pos' if pos else '') + '.{}.csv'.format(size)
+    features = pd.read_csv(csv_name)
+
+    for i in range(size):
+        features[key + str(i)] = features[key + str(i)].astype(np.float32)
+
+    oncol = 'msno' if user else 'song_id' if not artist else 'artist_name'
+    set_size = len(combi)
+    entry_list = []
+    for i in range(set_size):
+        entry_list.append(combi.iloc[i:i+1])
+    print("Copies created")
+    start = time.time()
+    for entry in entry_list:
+        entry = do_merge(entry, features, 'inner', oncol)
+    elapsed_time = time.time() - start
+
+    del features
+    gc.collect()
+
+    print('Latent feature %s join in %f seconds rows %d throughput %f: ' % (
+        name, elapsed_time, len(combi), len(combi) / elapsed_time))
+
+    return combi
+
+
+def create_featureset(folder):
+    combi = load_combi_prep(folder=folder, split=None)
+
+    # partition data by time
+    combi['time_bin10'] = pd.cut(combi['time'], 10, labels=range(10))
+    combi['time_bin5'] = pd.cut(combi['time'], 5, labels=range(5))
+
+    # add latent features
+    combi = add_als(folder, combi, UF_SIZE, user=True, postfix='', artist=False)
+
+
+if __name__ == '__main__':
+    create_featureset("tests/test_resources/wsdm_cup_features/")
