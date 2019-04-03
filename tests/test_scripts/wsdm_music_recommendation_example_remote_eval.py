31c71c60e3f436c2044ce79f1e0672e4e803d1e4
Peter Kraft <petereliaskraft@gmail.com>
2019-04-03 10:57:04 -0700

    Fixed caching bug

--- tests/test_scripts/wsdm_music_recommendation_example_remote_eval.py
+++ tests/test_scripts/wsdm_music_recommendation_example_remote_eval.py
@@ -199,7 +199,7 @@ all_funcs = ["get_row_to_merge_features_uf", "get_row_to_merge_features_sf", "ge
              "get_row_to_merge_stabs_features", "get_row_to_merge_stypes_features", "get_row_to_merge_regs_features"]
 
 if args.caching is not None:
-    cached_funcs = remote_funcs
+    cached_funcs = all_funcs
     max_cache_size = args.caching
 else:
     cached_funcs = ()
@@ -211,7 +211,7 @@ else:
     costly_statements = ()
 
 
-@willump_execute(disable=args.disable, batch=False, cached_funcs=all_funcs, costly_statements=costly_statements,
+@willump_execute(disable=args.disable, batch=False, cached_funcs=cached_funcs, costly_statements=costly_statements,
                  eval_cascades=cascades, cascade_threshold=cascade_threshold, max_cache_size=max_cache_size,
                  async_funcs=remote_funcs)
 def do_merge(combi):
